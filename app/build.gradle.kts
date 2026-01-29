4b8caaea65d23e30897285c68778d6d708fe7643
Vijendra kelwa <vijendrakelwa@Vijendras-MacBook-Air.local>
2026-01-29 12:53:16 +0530

    chore: Bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vijendra.focuslauncher"
         minSdk = 26 // Android 8.0
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
